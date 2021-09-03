00aacd1d3f9935bf566d6853f794ab59492b29b1
StableGenius <fengdongfa1995@163.com>
2021-09-03 11:57:07 +0800

    st patches

--- st/config.def.h
+++ st/config.def.h
@@ -203,6 +203,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ControlMask,          XK_b,           kscrollup,      {.i = -1} },
+	{ ControlMask,          XK_f,           kscrolldown,    {.i = -1} },
 };
 
 /*
