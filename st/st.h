cc947662545d5f69fc4066acf2abccf00d5b5ba3
StableGenius <fengdongfa1995@163.com>
2021-09-04 12:51:55 +0800

    externalpipe failed

--- st/st.h
+++ st/st.h
@@ -83,6 +83,7 @@ void draw(void);
 
 void kscrolldown(const Arg *);
 void kscrollup(const Arg *);
+void externalpipe(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
