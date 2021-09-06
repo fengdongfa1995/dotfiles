76750a302df30ea8bb2fc07c7f393832cfb9b612
StableGenius <fengdongfa1995@163.com>
2021-09-06 18:23:11 +0800

    finish slstatus

--- slstatus/config.def.h
+++ slstatus/config.def.h
@@ -65,6 +65,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-  { cpu_perc, "CPU(%s%%)",    NULL    },
-	{ datetime, "%s",           "%F %T" },
+  { cpu_perc,     "CPU(%s%%) ",   NULL         },
+  { ram_perc,     "RAM(%s%%) ",   NULL         },
+  { netspeed_rx,  "Net(%s) ",     "enp3s0f0u8" },
+	{ datetime,     "%s",           "%F %T"      },
 };
